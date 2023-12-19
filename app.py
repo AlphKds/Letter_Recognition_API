51f8912ef7ed1fb531e631427ed2f1334863f89a
Alvien Cenna <pincenna@gmail.com>
2023-12-19 12:30:25 +0700

    fix: adjust the whole processing to the new model

--- app.py
+++ app.py
@@ -6,91 +6,89 @@ from keras.models import load_model
 from werkzeug.utils import secure_filename
 import cv2
 import imutils
-from imutils.contours import sort_contours
 
 app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-app.config['MODEL_FILE'] = 'OCR_Resnet.h5'
+app.config['MODEL_FILE'] = 'letrec_model.h5'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 model = load_model(app.config['MODEL_FILE'], compile=False)
 
-def preprocess_image(image_path):
-    img = cv2.imread(image_path)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blurred = cv2.GaussianBlur(img_gray, (5, 5), 0)
-    img_edged = cv2.Canny(img_blurred, 30, 150)
-    img_contours = cv2.findContours(img_edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    img_contours = imutils.grab_contours(img_contours)
-    img_contours = sort_contours(img_contours, method="left-to-right")[0]
-    chars = []
 
-    for c in img_contours:
+def sort_contours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+    key=lambda b:b[1][i], reverse=reverse))
+    return (cnts, boundingBoxes)
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+labels = [l for l in letters ]
+
+def get_letters(img):
+    letters = []
+    image = cv2.imread(img)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 30, 150)
+    
+    countours, _ = cv2.findContours(edged.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    mask = np.zeros(edged.shape[:2],dtype=np.uint8)
+    for c in countours:
+        if cv2.contourArea(c):
+            x,y,w,h  = cv2.boundingRect(c)
+            cv2.rectangle(mask,(x,y),(x+w,y+h),(255),-1)
+
+    cnts= cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    
+    cnts = imutils.grab_contours(cnts)
+    cnts = sort_contours(cnts, method="left-to-right")[0]
+    print(len(cnts))
+    for c in cnts:
         (x, y, w, h) = cv2.boundingRect(c)
+        print(x, y, w, h)
+        if (w >= 5) and (h >= 15):
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi = gray[y:y + h, x:x + w]
+            thresh = cv2.threshold(roi, 
+                                   0, 255,
+                                   cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            thresh = cv2.resize(thresh,
+                                (28, 28),
+                                interpolation = cv2.INTER_CUBIC)
+            thresh = thresh.astype("float32") / 255.0
+            thresh = np.expand_dims(thresh, axis=-1)
+            thresh = thresh.reshape(1,28,28,1)
+            ypred = model.predict(thresh)
+            x = labels[ypred.argmax()-1]
+            letters.append(x)
+    return letters, image
 
-        if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
-            img_roi = img_gray[y:y+h, x:x+w]
-            thresh = cv2.threshold(
-                img_roi, 0, 255,
-                cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+def get_word(letter):
+    word = "".join(letter)
+    return word
 
-            (tH, tW) = thresh.shape
-
-            if tW > tH:
-                thresh = imutils.resize(thresh, width=32)
-            else:
-                thresh = imutils.resize(thresh, height=32)
-
-            (tH, tW) = thresh.shape
-            dX = int(max(0, 32-tW) / 2.0)
-            dY = int(max(0, 32-tH) / 2.0)
-
-            img_padded = cv2.copyMakeBorder(thresh,
-                                            top=dY,
-                                            bottom=dY,
-                                            left=dX,
-                                            right=dX,
-                                            borderType=cv2.BORDER_CONSTANT,
-                                            value=(0, 0, 0))
-            img_padded = cv2.resize(img_padded, (32, 32))
-            img_padded = img_padded.astype("float32") / 255.0
-            img_padded = np.expand_dims(img_padded, axis=-1)
-            chars.append((img_padded, (x, y, w, h)))
-
-    boxes = [b[1] for b in chars]
-    chars = np.array([c[0] for c in chars], dtype="float32")
-
-    return boxes, chars
-
-    # img = img.resize((32, 32))
-    # img_array = np.asarray(img)
-    # img_array = np.expand_dims(img_array, axis=0)
-    # img_array = np.expand_dims(img_array, axis=-1)  
-    # img_array = img_array / 255.0 
-    # return img_array
 
 def predict_letter(image_path):
-    boxes, chars = preprocess_image(image_path)
-    prediction = model.predict(chars)
-    
-    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    labels = [l for l in letters ]
+    letters, images = get_letters(image_path)
+    prediction = get_word(letters) 
     
     output = ""
-    
-    for (pred, (x, y, w, h)) in zip(prediction, boxes):
-        i = np.argmax(pred)
-        prob = pred[i]
-        label = labels[i]
-        print(label, ': ', prob)
-        if prob > 0.85:
-            output += label
 
-    # predicted_class = np.argmax(prediction)
-    # predicted_letter = chr(ord('A') + predicted_class)
+    for index, letter in enumerate(prediction):
+        output += letter
+        if index != len(prediction)-1:
+            output += " "
+    
     print(output)
     return output
 
